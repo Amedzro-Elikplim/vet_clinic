21f7676965f701ae3602d49a958112a16f5f0286
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-16 20:53:19 +0000

    Add comments to queries

--- queries.sql
+++ queries.sql
@@ -7,18 +7,23 @@ SELECT * FROM animals WHERE neutered IN (true);
 SELECT * FROM animals WHERE name NOT IN('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+--set species column to unspecified and roll back
 BEGIN;
 UPDATE animals
 SET species = 'unspecified'
 ROLLBACK;
 
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN;
 UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon%';
+SET species = 'digimon' WHERE name LIKE '%mon%';
 
+--Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 UPDATE animals
-SET species = 'pokemon'
-WHERE species IS NULL;
+SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 
+--Delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
