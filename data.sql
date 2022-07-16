b1dca387153665ff4e423514b31dc6424effc271
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-16 23:00:17 +0000

    Insert species table data

--- data.sql
+++ data.sql
@@ -40,9 +40,9 @@ INSERT INTO owners(full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners(full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners(full_name, age) VALUES ('Jodie Whittaker', 38);
 
--- --insert data into species' table
--- BEGIN;
--- ALTER SEQUENCE species_id_seq RESTART WITH 1;
--- INSERT INTO species(name) VALUES('Pokemon');
--- INSERT INTO species(name) VALUES('Digimon');
--- COMMIT;
+--insert data into species' table
+BEGIN;
+ALTER SEQUENCE species_id_seq RESTART WITH 1;
+INSERT INTO species(name) VALUES('Pokemon');
+INSERT INTO species(name) VALUES('Digimon');
+COMMIT;
