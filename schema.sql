5dd3ad45620afe2bf7f91aaafa19b29b9c7218ce
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-16 19:12:33 +0000

    create animal table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
-/* Database schema to keep the structure of entire database. */
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(250) NOT NULL,
+	date_of_birth DATE NOT NULL,
+	escape_attempts INT NOT NULL,
+	neutered bool,
+	weight_kg DECIMAL(6,2)
+);
