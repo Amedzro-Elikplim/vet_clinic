880a2150e19c475a5b70e7f06cd885796255d554
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-16 23:29:52 +0000

    Set owner id of animals

--- data.sql
+++ data.sql
@@ -46,3 +46,36 @@ ALTER SEQUENCE species_id_seq RESTART WITH 1;
 INSERT INTO species(name) VALUES('Pokemon');
 INSERT INTO species(name) VALUES('Digimon');
 COMMIT;
+
+
+BEGIN;
+UPDATE animals
+--set species_id for digimon
+SET species_id = 1 WHERE name LIKE '%mon%';
+
+UPDATE animals
+--set species_id for pokemon
+SET species_id = 2 WHERE species_id is NULL;
+COMMIT;
+
+BEGIN;
+--set sam smith's owner id
+UPDATE animals
+SET owner_id = 1 WHERE name IN ('Agumon');
+
+--set jennifer orwell's owner id
+UPDATE animals
+SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+
+--set Bob's owner id
+UPDATE animals
+SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+
+--set melody id
+UPDATE animals
+SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+--set dean id
+UPDATE animals
+SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+COMMIT;
