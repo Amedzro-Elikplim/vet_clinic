cd7d361aa95cc7eb2561e200850df18fa6b00856
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-16 19:37:38 +0000

    Add extra query

--- queries.sql
+++ queries.sql
@@ -4,4 +4,5 @@ SELECT * FROM animals WHERE neutered IN (true) AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered IN (true);
+SELECT * FROM animals WHERE name NOT IN('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
